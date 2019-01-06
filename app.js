16360f3da87d94cec51f82f8a331227dacb96ae1
harshmv <vaghadiahv@gmail.com>
2019-01-06 14:54:27 +0530

    First-Commit

---
+++ app.js
@@ -0,0 +1,42 @@
+var LCM = document.getElementById("lcm");
+var HCF = document.getElementById("hcf");
+
+LCM.onclick = function get() {
+    var a = document.getElementById("first").value;
+    var b = document.getElementById("second").value;
+    
+    function lcm(a, b) {
+       return (!a || !b) ? 0 : Math.abs((a * b) / hcf(a, b));
+     }
+    
+     function hcf(x, y) {
+        x = Math.abs(x);
+        y = Math.abs(y);
+        while(y) {
+          var t = y;
+          y = x % y;
+          x = t;
+        }
+        return x;
+      }
+     document.getElementById("alcm").innerText = `The LCM of ${a} and ${b} is ${lcm(a, b)}`;
+}
+ 
+HCF.onclick = function() {
+    var a = document.getElementById("first").value;
+    var b = document.getElementById("second").value;
+ 
+   function hcf(a, b) {
+   a = Math.abs(a);
+   b = Math.abs(b);
+   while(b) {
+     var t = b;
+     b = a % b;
+     a = t;
+   }
+   return a;
+ }
+ document.getElementById("ahcf").innerText = `The HCF of ${a} and ${b} is ${hcf(a, b)}`;
+}
+
+
